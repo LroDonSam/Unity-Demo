c5fb14ab253381f141ce76791ead807a77ee5535
astroboya2 <49719394+astroboya2@users.noreply.github.com>
2019-08-15 18:14:51 +0800

    1 角色移动

---
+++ Unity/Assets/Scripts/角色控制器.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 角色控制器 : MonoBehaviour
+{
+    private new Transform transform;
+    public float 移动速度 = 2;
+    public float 奔跑速度 = 6;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+    
+    void Update()
+    {
+        //控制移动
+        float appSpeed = 移动速度;
+        if (Input.GetKey(KeyCode.LeftShift))
+            appSpeed = 奔跑速度;
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * appSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * appSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * appSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * appSpeed * Time.deltaTime);
+    }
+}
---
+++ Unity/Assets/Scripts/跟随物体.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 跟随物体 : MonoBehaviour
+{
+    private new Transform transform;
+    public Transform 目标;
+    private Vector3 distance;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        distance = 目标.position - transform.position;
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = 目标.position - distance;
+    }
+}
